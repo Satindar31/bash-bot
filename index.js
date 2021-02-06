ff69309443bea1845da31381cca63dcbd73c160e
extremepro999 <73517520+extremepro999@users.noreply.github.com>
2021-02-06 23:05:10 +0530

    added third game

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ bot.on('ready', () => {
     }
 
     if(message.content.startsWith('d!play3')) {
-      message.channel.send(`🆗 ${message.author} here is the link to the third game https://rb.gy/o2krdf`)
+      message.channel.send(`🆗 ${message.author} here is the link to the third game rb.gy/o2krdf`)
     }
 
 
